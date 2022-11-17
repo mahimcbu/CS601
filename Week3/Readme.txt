bae972e83a3c1610d2adb266539a663aee6d4f4e
Mahim C <mahimc@bu.edu>
2022-11-17 18:15:42 -0500

    hw3 final

---
+++ Week3/Readme.txt
@@ -0,0 +1,8 @@
+Hi Josh,
+This week I created a webpage that uses javascript to do some basic operations.
+I created the html page that links the script.js at the end of part of body. 
+It starts of with a pop up welcome message then promts the user for a first name before popping another window to 
+welcome the visitor by name. It then asks for two numbers in two different prompts and then prints out the addition value.
+I used a function to perform the addition and another one to check for the total value against the number 10.
+Then we entered a while loop where based on the prompt respose of yes/no we do the same addion function or say bye to the user.
+
--- Week3/week3script.js
+++ Week3/week3script.js
@@ -1,31 +1,38 @@
 
-alert("Hello to my javascript!")
-let visitor = prompt("What's your first name?");
-if (visitor !=null){
-    alert("Let's play with some javascript "+visitor+"!");
+alert("Hello to my javascript!") //welcomes the visitor
+
+while(true){    //promts for a name. if none provided, keep asking
+    let visitor = prompt("What's your first name?");
+    if (visitor.length != 0){
+        alert("Let's play with some javascript "+visitor+"!"); //welcomes the visitor by name
+        break;
+    }else{
+        alert("Invalid input! Please type something.")
+    }
 }
-addition();
-while (true){
+
+addition(); //initially call the this function to do the addition of two numbers
+while (true){ //then keep asking the visitor if they want to add more
     let askAgain = prompt("Do you want to add two numbers again?(yes/no)");
     askAgain = askAgain.toLowerCase();
-    if (askAgain == String("yes")){
+    if (askAgain == String("yes")){ //if yes, call addition again
         addition();
     }
-    else if(askAgain == String("no")){
+    else if(askAgain == String("no")){ //if no, bye.
         alert("Thank you for visiting us "+visitor+"!");
         break;
     }else{
         alert("Invaild input");
     }
 }
-function addition(){
+function addition(){ //function that gets two values from prompt and add them in total variable
     firstNumber = Number(prompt("Enter your first number:"));
     seconNumber = Number(prompt("Enter your second number:"));
     total = (firstNumber+seconNumber);
     alert("The sum of your two numbers is: "+total);
-    compareTotal(total);
+    compareTotal(total); //calls the function by passing total value to check against the number 10
 }
-function compareTotal(total){
+function compareTotal(total){ //does the checking and prints out the comments
     if (total > 10){
         alert("That is a big number!");
     }else if (total <=10) {
